b1af5eb5dee478e67710e447fa73af48ae8dea77
Borys-Samoilenko <borys.samoilenko@gmail.com>
2021-09-04 15:42:43 +0300

    Updates

---
+++ static/js/main.fa4e4390.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-33"]=this["webpackJsonpreact-33"]||[]).push([[0],{3:function(s,t,c){"use strict";c.r(t);c(4),c(0)}},[[3,1,2]]]);
+//# sourceMappingURL=main.fa4e4390.chunk.js.map
